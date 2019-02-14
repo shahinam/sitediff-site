68ae9e35a97bbc28aca27e509c1454b05e5e3905
Renan Dias <renan.ferreira@hs2solutions.com>
2019-02-13 23:48:50 -0200

    Swipe in testimonials

--- gulpfile.js
+++ gulpfile.js
@@ -35,7 +35,8 @@
       },
       vendors: [
         'node_modules/jquery/dist/jquery.min.js',
-        'node_modules/bootstrap/dist/js/bootstrap.min.js'
+        'node_modules/bootstrap/dist/js/bootstrap.min.js',
+        'node_modules/swiper/dist/js/swiper.min.js'
       ]
     };
 
--- src/js/script.js
+++ src/js/script.js
@@ -5,27 +5,51 @@
 (function (window, $) {
   'use strict';
 
+  // Testimonials Swiper on mobile.
+  $(document).ready(function () {
+    //initialize swiper when document ready
+    var mySwiper = new Swiper ('.swiper-container', {
+      // Optional parameters
+      direction: 'horizontal',
+      loop: true,
+      breakpoints: {
+		768: {
+          slidesPerView: 1,
+          noSwiping: true,
+          allowSlidePrev: true,
+          allowSlideNext: true
+		},
+		992: {
+          slidesPerView: 2,
+          noSwiping: true,
+          allowSlidePrev: true,
+          allowSlideNext: true
+		},
+		9999: {
+          slidesPerView: 3,
+          noSwiping: false,
+          allowSlidePrev: false,
+          allowSlideNext: false
+		}
+	  }
+    })
+  });
+
   // Equal height for testimonial texts.
   testimonialsEqualHeight();
-  $(window).resize(testimonialsEqualHeight);
 
   function testimonialsEqualHeight() {
-    // Only apply for bootstrap breakpoint ls and above.
-    if ($(window).width() > 992) {
-      $('#testimonials').each(function () {
-        var max_height = 0;
-        // Get the height of the talles element.
-        $('.testimonial--testimonial', this).each(function () {
-          if ($(this).height() > max_height) {
-            max_height = $(this).height();
-          }
-        });
-        // Apply max height to all other elements.
-        $('.testimonial--testimonial', this).height(max_height);
+    $('#testimonials').each(function () {
+      var max_height = 0;
+      // Get the height of the talles element.
+      $('.testimonial--testimonial', this).each(function () {
+        if ($(this).height() > max_height) {
+          max_height = $(this).height();
+        }
       });
-    } else {
-      $('#testimonials .testimonial--testimonial').removeAttr('style');
-    }
+      // Apply max height to all other elements.
+      $('.testimonial--testimonial', this).height(max_height);
+    });
   }
 
 }(window, jQuery));
