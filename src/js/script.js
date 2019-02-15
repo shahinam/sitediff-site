d3d86c5b54af1078e611662540487cdea6660625
Renan Dias <renan.ferreira@hs2solutions.com>
2019-02-15 04:18:43 -0200

    Testimonials swiper navigation & CSS equal height

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,5 @@
 /*global
-    window, document, jQuery
+    window, document, jQuery, Swiper
 */
 
 (function (window, $) {
@@ -8,48 +8,39 @@
   // Testimonials Swiper on mobile.
   $(document).ready(function () {
     //initialize swiper when document ready
-    var mySwiper = new Swiper ('.swiper-container', {
-      // Optional parameters
+    var mySwiper = new Swiper('.swiper-container', {
       direction: 'horizontal',
       loop: true,
+      autoheight: true,
+      // Navigation arrows
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+      },
       breakpoints: {
-		768: {
+        768: {
           slidesPerView: 1,
+          slidesPerGroup: 1,
           noSwiping: true,
           allowSlidePrev: true,
           allowSlideNext: true
-		},
-		992: {
+        },
+        992: {
           slidesPerView: 2,
+          slidesPerGroup: 1,
           noSwiping: true,
           allowSlidePrev: true,
           allowSlideNext: true
-		},
-		9999: {
+        },
+        9999: {
           slidesPerView: 3,
+          slidesPerGroup: 1,
           noSwiping: false,
           allowSlidePrev: false,
           allowSlideNext: false
-		}
-	  }
-    })
-  });
-
-  // Equal height for testimonial texts.
-  testimonialsEqualHeight();
-
-  function testimonialsEqualHeight() {
-    $('#testimonials').each(function () {
-      var max_height = 0;
-      // Get the height of the talles element.
-      $('.testimonial--testimonial', this).each(function () {
-        if ($(this).height() > max_height) {
-          max_height = $(this).height();
         }
-      });
-      // Apply max height to all other elements.
-      $('.testimonial--testimonial', this).height(max_height);
+      }
     });
-  }
+  });
 
 }(window, jQuery));
